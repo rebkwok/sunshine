f0e086d807f92f83876a4a4f195b102690a7674b
Becky Smith <rebkwok@gmail.com>
2018-03-17 11:20:17 +0000

    Replace fb like buttons

--- website/static/website/bootstrap/custom.js
+++ website/static/website/bootstrap/custom.js
@@ -1,23 +1,4 @@
 
-<!-- Custom JavaScript for the facebook buttons -->
-
-//(function(d, s, id) {
-//      var js, fjs = d.getElementsByTagName(s)[0];
-//      if (d.getElementById(id)) return;
-//      js = d.createElement(s); js.id = id;
-//      js.src = "http://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.0";
-//      fjs.parentNode.insertBefore(js, fjs);
-//    }(document, 'script', 'facebook-jssdk'));
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.4&appId=1657101174502021";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
 <!-- Custom JavaScript for the sidebar Menu Toggle on the timetable page -->
 $("#menu-toggle").click(function(e) {
     e.preventDefault();
