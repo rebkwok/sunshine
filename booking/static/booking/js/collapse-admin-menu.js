210059ea75c1ad1df03f50fef003e9840a012a81
Becky Smith <rebkwok@gmail.com>
2020-02-23 13:45:28 +0000

    Collapsible admin menu

---
+++ booking/static/booking/js/collapse-admin-menu.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function()  {
+    var element = document.getElementById("suit-nav");
+    element.classList.add("collapse");
+    var subnav = document.getElementById("suit-sub-nav");
+    subnav.setAttribute("style", "line-height: 1;");
+});
