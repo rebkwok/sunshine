a443de3c5b8a010a323f5a4e442a7b8db839a9ec
Becky Smith <rebkwok@gmail.com>
2025-01-25 15:29:44 +0000

    Hide all locations references if there's only one visible location

--- timetable/context_processors.py
+++ timetable/context_processors.py
@@ -3,8 +3,14 @@ from .models import Venue, TimetableSession
 
 def timetable(request):
     active_locations = TimetableSession.active_locations()
+    if active_locations.count() > 1:
+        return {
+            'timetable_locations': [
+                (tab, loc) for tab, loc in Venue.location_choices().items() if (tab == 0 or loc in active_locations)
+            ]
+        }
     return {
-        'timetable_locations': [
-            (tab, loc) for tab, loc in Venue.location_choices().items() if (tab == 0 or loc in active_locations)
-        ]
-    }
+            'timetable_locations': [
+                (tab, loc) for tab, loc in Venue.location_choices().items() if loc in active_locations
+            ]
+        }
