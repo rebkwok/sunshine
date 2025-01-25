4d81b826d90cdb49877bb3d2875592178a20d585
Becky Smith <rebkwok@gmail.com>
2025-01-25 15:42:06 +0000

    Placeholder venue and session type pages

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from website.views import home, contact, contact_form, faq
+from website.views import home, contact, contact_form, faq, session_types, venues
 
 app_name = 'website'
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('contact-form/', contact_form, name='contact_form'),
     path('contact/', contact, name='contact'),
     path('faq/', faq, name='faq'),
+    path("what-we-offer/", session_types, name="session_types"),
+    path("venues/", venues, name="venues"),
 ]
