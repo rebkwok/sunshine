2f5670359a2595d38d33eb1bd298deadec946c0e
Becky Smith <rebkwok@gmail.com>
2026-01-02 12:13:01 +0000

    Put new homepage behind feature flag

--- website/urls.py
+++ website/urls.py
@@ -1,13 +1,18 @@
+from django.conf import settings
 from django.urls import path
 
-from website.views import home, contact, faq, session_types, venues
+from website.views import home, home_legacy, contact, faq, session_types, venues
 
 app_name = 'website'
 
 urlpatterns = [
-    path('', home, name='home'),
     path('contact/', contact, name='contact'),
     path('faq/', faq, name='faq'),
     path("what-we-offer/", session_types, name="session_types"),
     path("venues/", venues, name="venues"),
 ]
+
+if settings.LEGACY_HOMEPAGE:
+    urlpatterns.insert(0, path('', home_legacy, name='home'))
+else:
+    urlpatterns.insert(0, path('', home, name='home'))
